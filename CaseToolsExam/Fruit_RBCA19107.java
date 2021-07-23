5431baa6befea0dfd7a10bb950a226c39590c359
Fernandes <Fernandes@LAPTOP-ST3Q02VT>
2021-07-23 15:03:33 +0530

    branchx changes

---
+++ CaseToolsExam/Fruit_RBCA19107.java
@@ -0,0 +1,20 @@
+
+public class Fruit_RBCA19107 {
+	
+	String color;
+	Fruit_RBCA19107(String color)
+	{
+	this.color=color;
+	}
+	
+	String get_color()
+	{
+	return color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
