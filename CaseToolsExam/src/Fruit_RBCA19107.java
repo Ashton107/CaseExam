b6f3022f6d113d767d05847748c0376b1938cd00
Fernandes <Fernandes@LAPTOP-ST3Q02VT>
2021-07-23 14:51:02 +0530

    First commit

---
+++ CaseToolsExam/src/Fruit_RBCA19107.java
@@ -0,0 +1,15 @@
+
+public class Fruit_RBCA19107 {
+	
+	String color;
+	Fruit_RBCA19107(String color)
+	{
+	this.color=color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
